d1f5c9f2a050615e57cacdac8da9d848f9aa3dbc
FrozenC <caoyi0905@163.com>
2016-06-30 12:33:13 +0800

    测试 JUnit spring-test

---
+++ src/main/java/com/wisely/highlight_spring4/ch3/fortest/TestBean.java
@@ -0,0 +1,20 @@
+package com.wisely.highlight_spring4.ch3.fortest;
+
+/**
+ * Created by FrozenC on 2016/6/30.
+ */
+public class TestBean {
+    private String content;
+    public TestBean(String content) {
+        super();
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
